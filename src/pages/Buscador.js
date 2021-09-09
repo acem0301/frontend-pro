3c3a9f1784afd47ea5508c0e4aefb437f42e4340
acem0301 <acem0301@gmail.com>
2021-09-08 23:04:44 -0400

    Avance buscador

--- src/pages/Buscador.js
+++ src/pages/Buscador.js
@@ -1,22 +1,14 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import NativeSelect from "@material-ui/core/NativeSelect";
+import "./styles/Buscador.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -94,11 +86,37 @@ export default function SignInSide() {
                 autoComplete="email"
                 autoFocus
               />
-              <Grid xs={12} sm={12} md={8}>
+              <Grid xs={12} sm={12} md={12}>
                 <FormControl
                   variant="outlined"
                   className={classes.formControl}
-                  md={8}
+                  md={6}
+                >
+                  <InputLabel htmlFor="outlined-age-native-simple">
+                    Ciudad
+                  </InputLabel>
+                  <Select
+                    native
+                    value={state.age}
+                    onChange={handleChange}
+                    label="Age"
+                    inputProps={{
+                      name: "age",
+                      id: "outlined-age-native-simple",
+                    }}
+                  >
+                    <option aria-label="None" value="" />
+                    <option value={1}>Asunción</option>
+                    <option value={2}>Villa Elisa</option>
+                    <option value={3}>Lambaré</option>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid xs={6} sm={6} md={6}>
+                <FormControl
+                  variant="outlined"
+                  className={classes.formControl}
+                  md={6}
                 >
                   <InputLabel htmlFor="outlined-age-native-simple">
                     Ciudad
